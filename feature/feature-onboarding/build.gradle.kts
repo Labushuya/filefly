7452986e80028f404b2826e36a10756b60d43bd6
Bott <christopher.bott@sap.com>
2026-09-13 18:52:09 +0200

    fix(build): add missing core-data dependency to feature-onboarding

--- feature/feature-onboarding/build.gradle.kts
+++ feature/feature-onboarding/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 dependencies {
     implementation(project(":core:core-common"))
     implementation(project(":core:core-network"))
+    implementation(project(":core:core-data"))
     implementation(project(":core:core-ui"))
     implementation(libs.kotlinx.coroutines.android)
 }
